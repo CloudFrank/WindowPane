005c33f5f8a65ebde92215d17f36a52159b560f1
CloudFrank.com <57855650+CloudFrank@users.noreply.github.com>
2020-01-09 07:49:17 +0800

    Create WindowPane.js

---
+++ WindowPane.js
@@ -0,0 +1,74 @@
+export default class WindowPane extends HTMLDivElement {
+  constructor() {
+    super();
+    this.attachShadow({mode: 'open'});
+    this.render();
+  }
+  
+  render() {
+    this.shadowRoot.innerHTML = this.template();
+  }
+  
+  template() {
+    return `
+      <slot></slot>
+      <style>
+        ${this.styleHost()}
+        ${this.stylePane()}
+      </style>
+    `;
+  }
+  
+  styleHost() {
+    return `
+      :host {
+        display: grid;
+        grid-template-columns: max-content 1fr max-content;
+        grid-template-rows: max-content 1fr max-content;
+        overflow: hidden;
+      }
+      :host(.viewport) {
+        width: 100vw;
+        height: 100vh;
+      }
+    `;
+  }
+  
+  stylePane() {
+    return `
+      ::slotted(.left) {
+        grid-column: 1 / 2;
+        grid-row: 1 / -1;
+        background-color: green;
+      }
+      ::slotted(.right) {
+        grid-column: -2 / -1;
+        grid-row: 1 / -1;
+        background-color: green;
+      }
+      ::slotted(.top) {
+        grid-column: 1 / -1;
+        grid-row: 1 / 2;
+        background-color: pink;
+      }
+      ::slotted(.bottom) {
+        grid-column: 1 / -1;
+        grid-row: -1 / -2;
+        background-color: pink;
+      }
+      ::slotted(.main) {
+        grid-column: 2 / -2;
+        grid-row: 2 / -2;
+      }
+      ::slotted(.middle) {
+        grid-column: 1 / -1;
+        grid-row: 2 / -2;
+      }
+      ::slotted(.center) {
+        grid-column: 2 / -2;
+        grid-row: 1 / -1;
+      }
+    `;
+  }
+  
+};
