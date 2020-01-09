33eb1bbf551d11209a750ad4402289bc16fadd30
CloudFrank.com <57855650+CloudFrank@users.noreply.github.com>
2020-01-09 08:31:38 +0800

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-import WindowPane from './WindowPane.js';
-
-import ScrollPane from './ScrollPane.js';
-
-customElements.define('window-pane', WindowPane, { extends: "div"});
-customElements.define('scroll-pane', ScrollPane, { extends: "div"});
