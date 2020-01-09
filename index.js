c237fdab211b4de6832ab7d71b4fd01a06d4ffff
CloudFrank.com <57855650+CloudFrank@users.noreply.github.com>
2020-01-09 08:43:01 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+import WindowPane from 'https://cloudfrank.github.io/WindowPane/WindowPane.js';
+customElements.define('window-pane', WindowPane, { extends: "div"});
+import ScrollPane from 'https://cloudfrank.github.io/WindowPane/ScrollPane.js';
+customElements.define('scroll-pane', ScrollPane, { extends: "div"});
